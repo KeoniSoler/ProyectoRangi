802d20b8b407b16ba0f6c024cfdad1c62008cfc9
KeoniSoler <ksoler@udesa.edu.ar>
2023-06-08 15:46:17 -0300

    Arreglando

--- js/generos.js
+++ js/generos.js
@@ -5,14 +5,14 @@ fetch (url)
     return response.json ()
 })
 .then (function (info){
-    console.log(info)
+    console.log(info.data)
     let genero = document.querySelector (".genero")
     let todoslosgeneros = []
     for (let i = 1; i < info.data.length; i++){
         todoslosgeneros +=
         `<article>
-        <img src= "${info.data[i].picture}" alt="" />
-        <p>Name:  ${info.data[i].name}  </p>
+        <img src= "${info.data[i].picture_medium}" alt="" />
+        <p> ${info.data[i].name}  </p>
         </article>`
     }
     genero.innerHTML= todoslosgeneros
