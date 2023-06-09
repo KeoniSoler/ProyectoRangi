f144e42453eebf485ba555d4e4b92b9d3132e157
KeoniSoler <ksoler@udesa.edu.ar>
2023-06-09 10:30:31 -0300

    Dark Mode y Albumes editado x todos

--- js/index.js
+++ js/index.js
@@ -1,28 +1,5 @@
-fetch (url)
-.then(function(response){
-    return response.json ()
-})
-.then (function (info){
-    console.log(info)
-    let genero = document.querySelector (".genero")
-    let listageneros = []
-    for (let i = 1; i < info.data.length; i++){
-        listageneros +=
-        `<article>
-        <img src= "${info.data[i].picture}" alt="" />
-        <p>Name:  ${info.data[i].name}  </p>
-        </article>`
-    }
-    genero.innerHTML= listageneros
-    console.log(listageneros);
-})
-.catch (function (error){
-    alert (error);
-    console.log('Error:', error);
-})
-
-const checkbox = document.getElementById('dark-mode-checkbox');
-const body = document.body;
+let checkbox = document.getElementById('dark-mode-checkbox');
+let body = document.body;
 
 checkbox.addEventListener('change', function () {
   if (this.checked) {
