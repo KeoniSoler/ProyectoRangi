44d9cf89ef5b67bc4354a8caae19a183bbc519db
KeoniSoler <ksoler@udesa.edu.ar>
2023-06-09 12:23:40 -0300

    Arrancando devuelta

--- js/index.js
+++ js/index.js
@@ -8,3 +8,31 @@ checkbox.addEventListener('change', function () {
     body.classList.remove('dark-mode');
   }
 });
+
+// Tracks
+
+// Artistas
+let url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artists"
+
+fetch (url)
+.then(function(response){
+    return response.json ()
+})
+.then (function (info){
+    console.log(info.data)
+    let artista = document.querySelector (".artista")
+    let todoslosartistas = []
+    for (let i = 1; i < info.data.length; i++){
+        todoslosartistas +=
+        `<article>
+        <img src= "${info.data[i].picture_medium}" alt="" />
+        <p>Name:  ${info.data[i].name}  </p>
+        </article>`
+    }
+    artista.innerHTML= todoslosartistas
+    console.log(todoslosartistas);
+})
+.catch (function (error){
+    alert (error)
+})
+// Albumes
